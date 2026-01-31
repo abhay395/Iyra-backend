23b44a775d935dec8ffe8366db4180e4157dd6a0
Abhay Prajapati <abhayparja90@gmail.com>
2026-01-31 16:44:18 +0530

    Test

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const compression = require("compression");
-const connectDB = require("./config/db");
-const errorHandler = require("./middleware/error.middleware");
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const helmet = require("helmet");
+// const morgan = require("morgan");
+// const compression = require("compression");
+// const connectDB = require("./config/db");
+// const errorHandler = require("./middleware/error.middleware");
 
-const app = express();
+// const app = express();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || "*",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || "*",
+//   credentials: true,
+// }));
 
-app.use(helmet());
-app.use(compression());
-app.use(morgan("dev"));
+// app.use(helmet());
+// app.use(compression());
+// app.use(morgan("dev"));
 
-app.use("/api/blogs", require("./routes/blog.routes"));
+// app.use("/api/blogs", require("./routes/blog.routes"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Blog API");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Blog API");
+// });
 
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
-});
+// app.use((req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found",
+//   });
+// });
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
-const startServer = async () => {
-  await connectDB();
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
-};
+// const startServer = async () => {
+//   await connectDB();
+//   app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+//   });
+// };
 
-startServer();
+// startServer();
 
-module.exports = app;
+// module.exports = app;
